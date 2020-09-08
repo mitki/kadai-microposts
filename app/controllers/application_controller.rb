4a6af4f3f84d8105b7b9694e97d13f7e212ae3f3
mitsuki <apple.maroru03@gmail.com>
2020-09-08 02:54:10 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  include SessionsHelper
+    
+  private
+    
+  def require_user_logged_iin
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
